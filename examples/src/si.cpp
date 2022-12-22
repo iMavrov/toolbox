037ccfb27fac8e55d941217c3b70fa3f67ae0993
Ivan Mavrov <ivanmavrov@gmail.com>
2022-12-22 10:40:09 +0200

    Add documentation, type check and common units of measure

--- examples/src/si.cpp
+++ examples/src/si.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
 
-#include "si/unit.hpp"
+#include "si/common_units.hpp"
 
 using namespace toolbox::si;
-
-typedef Unit<float, 0, 0, 0> Measureless;
-typedef Unit<float, 1, 0, 0> Duration;
-typedef Unit<float, 0, 1, 0> Distance;
-typedef Unit<float, 0, 0, 1> Weight;
-
-typedef Unit<float, 0, 2, 0> Area;
-typedef Unit<float, 0, 3, 0> Volume;
-
-typedef Unit<float, -1, 1, 0> Velocity;
-typedef Unit<float, -2, 1, 0> Acceleration;
 
 typedef Unit<float, 1, -1, 0> Pace;
 
--- include/si/unit.hpp
+++ include/si/unit.hpp
@@ -7,35 +7,44 @@ namespace toolbox {
 
 namespace si {
 
+/// @brief Implements units of measure semantics to simple numerical values.
+/// Uses SI base units of measure and their combinations.
+///
+/// @tparam T The numerical value type to store. Floating point type expected.
+/// @tparam S Power of seconds.
+/// @tparam M Power of meters.
+/// @tparam KG Power of kilograms.
 template <typename T, int S, int M, int KG>
 class Unit {
 
 public:
+  static_assert(std::is_floating_point<T>::value, "Unit expects a floating point numeric type for 'T'");
+
   Unit() : value(0) { }
 
   explicit Unit(const T &value) : value(value) { }
 
-  const T &get() const { return value; }
+  const T &Get() const { return value; }
 
   Unit operator+(const Unit &rhs) const {
-    return Unit(value + rhs.get());
+    return Unit(value + rhs.Get());
   }
 
   Unit operator-(const Unit &rhs) const {
-    return Unit(value - rhs.get());
+    return Unit(value - rhs.Get());
   }
 
   template<int OtherS, int OtherM, int OtherKG>
   Unit<T, S + OtherS, M + OtherM, KG + OtherKG> operator*(const Unit<T, OtherS, OtherM, OtherKG> &rhs) const {
-    return Unit<T, S + OtherS, M + OtherM, KG + OtherKG>(value * rhs.get());
+    return Unit<T, S + OtherS, M + OtherM, KG + OtherKG>(value * rhs.Get());
   }
 
   template<int OtherS, int OtherM, int OtherKG>
   Unit<T, S - OtherS, M - OtherM, KG - OtherKG> operator/(const Unit<T, OtherS, OtherM, OtherKG> &rhs) const {
-    return Unit<T, S - OtherS, M - OtherM, KG - OtherKG>(value / rhs.get());
+    return Unit<T, S - OtherS, M - OtherM, KG - OtherKG>(value / rhs.Get());
   }
 
-  std::string toString() const {
+  std::string ToString() const {
     std::stringstream buffer;
     buffer << *this;
     return buffer.str();
@@ -61,7 +70,7 @@ static void PrintUnit(std::ostream& stream, const char *unit, int power, bool nu
 
 template <typename T, int S, int M, int KG>
 std::ostream& operator<<(std::ostream& stream, const Unit<T, S, M, KG> &other) {
-  stream << other.get();
+  stream << other.Get();
   
   if (S != 0 || M != 0 || KG != 0) {
     stream << ' ';
